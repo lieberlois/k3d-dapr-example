f2fc0f0f9e164361d7cec53360e40ecf9009a372
Luis Schweigard <luis.schweigard@gmail.com>
2022-05-03 08:24:04 +0200

    Using In-Memory-Database for Development

--- PostsService/Program.cs
+++ PostsService/Program.cs
@@ -15,20 +15,38 @@ builder.Services.AddScoped<IPostsPublishService, PostsPublishService>();
 builder.Services.AddScoped<IUrlService, UrlService>();
 
 // EF Core
-var connectionString = builder.Configuration.GetConnectionString("PostgreSQL");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
-
-// Migrations
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
+if (builder.Environment.IsDevelopment())
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    Console.WriteLine("[INFO] Using In-Memory-Database");
+    builder.Services.AddDbContext<AppDbContext>(
+        options => options.UseInMemoryDatabase(
+            databaseName: "TestDB"
+        )
+    );
+}
+else
+{
+    Console.WriteLine("[INFO] Using Postgres-Database");
+    var connectionString = builder.Configuration.GetConnectionString("PostgreSQL");
+    builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 }
 
 // PostgreSQL Container:
 // docker run -it --env POSTGRES_USER=root --env POSTGRES_PASSWORD=root --env POSTGRES_DB=posts_db -p 5432:5432 postgres:latest
+
+var app = builder.Build();
+
+// Run migration in production mode
+if (!app.Environment.IsDevelopment())
+{
+    Console.WriteLine("Migrating database...");
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.Migrate();
+    }
+}
 
 
 // Configure the HTTP request pipeline.
